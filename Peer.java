efc23769db3ce78266dd3f82abb4db5699e046d8
revav18 <rvijayn@ufl.edu>
2023-10-23 17:37:46 -0400

    Added PeerProcess.java, Peer.java files

---
+++ Peer.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Peer 
+{
+    int peerId;
+    String hostname;
+    int port;
+    int bitfield;
+    String numPrefNeighbors;
+    int unchokingInterval;
+    int optUnchokingInterval;
+    String filename;
+    long filesize;
+    long piecesize;
+
+    public Peer(int peerId, String hostname, int port, int bitfield)
+    {
+        this.peerId = peerId;
+        this.hostname = hostname;
+        this.port = port;
+        this.bitfield = bitfield;
+    } 
+
+    public void setCommonConfig() throws FileNotFoundException
+    {
+        File commonConfig = new File("Common.cfg");
+        Scanner scnr = new Scanner(commonConfig);
+
+        List<String> tempVariables = new ArrayList<String>();
+        while(scnr.hasNextLine())
+        {
+            String line = scnr.nextLine();
+            String [] commonvariables = line.split(" ");
+            tempVariables.add(commonvariables[1]);
+        }
+
+        scnr.close();
+        
+        this.numPrefNeighbors = tempVariables.get(0);
+        this.unchokingInterval = Integer.parseInt(tempVariables.get(1));
+        this.optUnchokingInterval = Integer.parseInt(tempVariables.get(2));
+        this.filename = tempVariables.get(3);
+        this.filesize = Integer.parseInt(tempVariables.get(4));
+        this.piecesize = Integer.parseInt(tempVariables.get(5));
+    }
+}
---
+++ PeerProcess.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+
+
+public class PeerProcess 
+{
+    public static void main(String arg[]) throws FileNotFoundException 
+    {
+      
+        File peerConfig = new File("PeerInfo.cfg");
+
+        Hashtable<Integer, Peer> Peers = new Hashtable<Integer, Peer>();
+
+        Scanner scnr = new Scanner(peerConfig);
+
+        while(scnr.hasNextLine())
+        {
+            String line = scnr.nextLine();
+            String [] variables = line.split(" ");
+            Peer peer = new Peer(Integer.parseInt(variables[0]), variables[1], Integer.parseInt(variables[2]), Integer.parseInt(variables[3]));
+            // System.out.println("Peer stuff" + peer.peerId + " " + peer.hostname + " " + peer.port + " " + peer.bitfield);
+            Peers.put(peer.peerId, peer);
+        }
+        scnr.close();
+    }
+}
