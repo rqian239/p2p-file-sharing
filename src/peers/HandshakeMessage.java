eb100b008bfe8d372d2e29b25a0511c614b0b2f7
Sonu Venu <sonu.venu@gmail.com>
2023-10-24 17:47:39 -0400

    Added handshake message to peers folder.

---
+++ src/peers/HandshakeMessage.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+
+
+// This is a class that holds the HandshakeMessage
+public class HandshakeMessage implements Serializable {
+    
+    private int peerID;
+    private char[] zeroBits;
+    private char[] handshakeHeader;
+
+    // Constructor: create a handshake message with a peerID
+    HandshakeMessage(int peerID) {
+        this.peerID = peerID;
+        this.zeroBits = Constants.getZERO_BITS().toCharArray();
+        this.handshakeHeader = Constants.getHANDSHAKE_HEADER().toCharArray();
+    }
+
+    // Getters
+
+    public int getPeerID() {
+        return peerID;
+    }
+
+    // Setters
+
+    public void setPeerID(int id) {
+        this.peerID = id;
+    }
+
+}
