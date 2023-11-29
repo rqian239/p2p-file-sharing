7c3b460cd73c8e2dc6469598c65b419d62570af2
Pranav Venu <sonu.venu@gmail.com>
2023-11-29 18:58:36 -0500

    added checkInterested

--- src/PeerProcess.java
+++ src/PeerProcess.java
@@ -4,13 +4,17 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.Properties;
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 
 public class PeerProcess {
 
     // Config filenames
-    static final String COMMON_CONFIG_FILE = "Common.cfg";
-    static final String PEER_INFO_CONFIG_FILE = "PeerInfo.cfg";
+    static final String COMMON_CONFIG_FILE = "../Common.cfg";
+    static final String PEER_INFO_CONFIG_FILE = "../PeerInfo.cfg";
 
 
 
@@ -23,6 +27,8 @@ public class PeerProcess {
 
         // Create a Peer object
         Peer peer = createPeer(peerID);
+        Map<Integer, AbstractMap.SimpleEntry<Peer, Socket>> peerMap = new HashMap<>();
+        
 
         // Parse Common.cfg file
         Properties properties = new Properties();
@@ -38,15 +44,10 @@ public class PeerProcess {
         int fileSize = Integer.parseInt(properties.getProperty("FileSize"));
         int pieceSize = Integer.parseInt(properties.getProperty("PieceSize"));
 
-
-
-
-
-
         // Start the peer process
         try {
             startPeerProcess(peer);
-            checkInterested(peer);
+            checkInterested(peer, peerMap);
         } catch (IOException e) {
             System.err.println("Error starting the peer process: " + e.getMessage());
             e.printStackTrace();
@@ -64,7 +65,7 @@ public class PeerProcess {
     }
 
     private static void startPeerProcess(Peer peer) throws IOException {
-        
+        System.out.println("Peer " + peer.getPeerID() + " starting...");
         Socket socket = new Socket(peer.getHostname(), peer.getPort());
         sendHandshake(socket, peer);
         sendBitfield(socket, peer);
@@ -103,11 +104,27 @@ public class PeerProcess {
         }
     }
 
-    private static void checkInterested(Peer peer){
+    private static void checkInterested(Peer peer, Map<Integer, AbstractMap.SimpleEntry<Peer, Socket>> peerMap){
         for(int i = 0; i < peer.getNumPieces(); i++){
-            if(peer.hasPiece(i) && ){
-                System.out.println("Peer " + peer.getPeerID() + " has piece " + i);
+            for(Map.Entry<Integer, AbstractMap.SimpleEntry<Peer, Socket>> entry : peerMap.entrySet()){
+                if(!peer.hasPiece(i) && entry.getValue().getKey().hasPiece(i)){
+                    System.out.println("Peer " + peer.getPeerID() + " is interested in " + entry.getValue().getKey().getPeerID());
+                    //send interested message
+                    try {
+                        messages.Message interested = new messages.Message((byte) 2);
+                        OutputStream outputStream;
+                        outputStream = entry.getValue().getValue().getOutputStream();
+                        outputStream.write(interested.createMessageBytes());
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    System.out.println("Peer " + peer.getPeerID() + " is not interested in " + entry.getValue().getKey().getPeerID());
+                }
             }
+
         }
     }
 }
