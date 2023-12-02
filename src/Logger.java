c58ca751e7f5049cdaf3660cbfebbf5321cde99e
revav18 <rvijayn@ufl.edu>
2023-12-02 00:44:33 -0500

    creates logger file for each peerID

--- src/Logger.java
+++ src/Logger.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -6,17 +9,24 @@ public class Logger {
 
 
     private static int currpeerID;
-    public Logger(int peerID) {
-        // this.currpeerID = peerID;
-        //eventually write to file
+    private PrintWriter logWriter;
 
+    public Logger(int peerID) {
+        this.currpeerID = peerID;        
         String logFileName = "log_peer_" + peerID + ".log";
+        try {
+            logWriter = new PrintWriter(new FileWriter(logFileName, true));
+        } 
+        catch (IOException e) {
+            System.out.println("failed to create logwriter");
+            e.printStackTrace();
+        }
 
     }
     
     //true for receivehandshake and false for sendhandshake pretty sure
     //can get rid of peerID1 if currpeerID will always be peerID1
-    public String logConnection(int peerID1, int peerID2, boolean connectedFrom) {
+    public void logConnection(int peerID1, int peerID2, boolean connectedFrom) {
         String logMessage;
         if (connectedFrom) {
             //receives handshake
@@ -26,11 +36,13 @@ public class Logger {
             //sendshandshake
             logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " makes a connection to Peer " + peerID2 + ".";
         }
-        return logMessage;    
+        logWriter.println(logMessage);
+        logWriter.flush();
+        // return logMessage;    
     }
 
     //pass in neighbors as ints, may change into peers later? 
-    public String logChangePreferredNeighbors(int peerID1, int[] preferredNeighborIDs) {
+    public void logChangePreferredNeighbors(int peerID1, int[] preferredNeighborIDs) {
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " has the preferred neighbors ";
         StringBuilder neighborsList = new StringBuilder();
         for (int i = 0; i < preferredNeighborIDs.length; i++) {
@@ -40,62 +52,65 @@ public class Logger {
             }
         }
         logMessage += "[" + neighborsList + "].";
-        return logMessage;
-
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
-    public String logChangeOptUnchokeNeighbor(int peerID1, int optimisticUnchokedNeighbor){
+    public void logChangeOptUnchokeNeighbor(int peerID1, int optimisticUnchokedNeighbor){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " has the optimistically unchoked neighbor " + optimisticUnchokedNeighbor;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
 
-    public String logReceiveUnchoke(int peerID1, int peerID2){
+    public void logReceiveUnchoke(int peerID1, int peerID2){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " is unchoked by " + peerID2;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
     
-    public String logReceiveChoke(int peerID1, int peerID2){
+    public void logReceiveChoke(int peerID1, int peerID2){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " is choked by " + peerID2;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
 
-    public String logReceiveHave(int peerID1, int peerID2, int pieceIndex){
+    public void logReceiveHave(int peerID1, int peerID2, int pieceIndex){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " received the 'have' message from " + peerID2 + " for the piece " + pieceIndex;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
 
-    public String logReceiveInterested(int peerID1, int peerID2){
+    public void logReceiveInterested(int peerID1, int peerID2){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " received the ‘interested’ message from " + peerID2;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
 
-    public String logReceiveNotInterested(int peerID1, int peerID2){
+    public void logReceiveNotInterested(int peerID1, int peerID2){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " received the ‘not interested’ message from " + peerID2;
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
 
-    public String logPieceDownloadedFrom(int peerID1, int peerID2, int pieceIndex, int totNumPieces){
+    public void logPieceDownloadedFrom(int peerID1, int peerID2, int pieceIndex, int totNumPieces){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + peerID1 + " has downloaded the piece " + pieceIndex + 
         " from "+ peerID2 + ". Now the number of pieces it has is " + totNumPieces + ".";
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
 
     }
 
-    public String logFileDownloaded(int peerID1){
+    public void logFileDownloaded(int peerID1){
         String logMessage = "[" + getCurrentTime() + "]: Peer " + currpeerID + " has downloaded the complete file.";
-        return logMessage;
+        logWriter.println(logMessage);
+        logWriter.flush();
     }
-
-
-
 
 
     public static String getCurrentTime() {
         // Get the current time
         LocalDateTime currentTime = LocalDateTime.now();
-
         // Define the desired date-time format
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-
         // Format the current time
         return currentTime.format(formatter);
     }
